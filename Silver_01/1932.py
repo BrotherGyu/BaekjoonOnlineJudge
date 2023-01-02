8a770dc15d1f235c56139f3ff9c64a77a17aa683
Brother_gyu <jang07133@gmail.com>
2023-01-02 14:08:57 +0900

    -

--- Silver_01/1932.py
+++ Silver_01/1932.py
@@ -5,6 +5,8 @@ file_dir_path=os.path.dirname(os.path.realpath(__file__))
 test_file_name='\\'+os.path.basename(__file__).replace('.py','.txt')
 sys.stdin = open(file_dir_path + test_file_name, "r")
 ######################
+import sys
+
 triangle_size = int(sys.stdin.readline())
 triangle_list = []
 for _ in range(triangle_size):
